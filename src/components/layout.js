e1e418d45c1d47b6e6399a9bc3f2376e483e731f
Adrianti Rusli <adriantirusli@gmail.com>
2020-01-24 23:27:02 +0700

    change header, fix menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Menu from "./Menu"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -34,11 +28,10 @@ const Layout = ({ children }) => {
           paddingTop: 0,
         }}
       >
+        <Menu />
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}, Dibangun dengan 💜
         </footer>
       </div>
     </>
